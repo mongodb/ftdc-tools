2f7d43c3bd4c8933d933d857db43ab18342bde26
Raiden Worley <carl.worley@mongodb.com>
2022-10-01 10:36:40 -0400

    basic commit

--- src/ftdc_tools/decoder.py
+++ src/ftdc_tools/decoder.py
@@ -1,334 +1,177 @@
 """Package to decode FTDC data."""
-import collections
-import struct
+import bson
+import io
 import zlib
+import datetime
+import requests
+import time
 
-from typing import (
-    AsyncGenerator,
-    AsyncIterator,
-    ByteString,
-    Dict,
-    Generator,
-    Iterable,
-    Iterator,
-    Tuple,
-    Union,
-)
-
-_int32 = struct.Struct("<i")
-_uint32 = struct.Struct("<I")
-_int64 = struct.Struct("<q")
-_uint64 = struct.Struct("<Q")
-_double = struct.Struct("<d")
+from typing import Any, Callable, List, Tuple
+from collections.abc import MutableMapping
+from dataclasses import dataclass
 
 
-class Chunk(collections.OrderedDict):
-    """Defines a chuck of BSON data."""
+# TODO: Generic?
+@dataclass
+class MetricChunk:
+    key_path: Any
+    values: List[Any]
+    reference_val: Any
+    delta_constructor: Callable[Any, Any]
 
-    bson_len: int
-    chunk_len: int
-    nsamples: int
+
+def decode_iter(ftdc):
+    """Iterate over all docs in a FTDC stream."""
+    for chunk in bson.decode_iter(ftdc):
+        for doc in _iter_chunk(chunk):
+            yield doc
 
 
-class FTDC(Iterable):
-    """
-    Defines a FTDC object that can decode binary FTDC data.
+def decode_file_iter(ftdc):
+    """Iterate over all docs in a FTDC stream."""
+    for chunk in bson.decode_file_iter(ftdc):
+        for doc in _iter_chunk(chunk):
+            yield doc
+    
+                    
+def _iter_chunk(chunk):
+    """Iterate over a single FTDC chunk."""
+    raw_data = chunk["data"]
 
-    The class is a iterate and take bystring or a Iterator/AsysnIterator as input. The object can be
-    iterated upon to get the decoded FTDC data.
-    """
+    # Remove 4 bytes to drop header, decompressing the rest.
+    bson_data = io.BytesIO(zlib.decompress(raw_data[4:]))
 
-    def __init__(
-        self, ftdc_data: Union[AsyncIterator, Iterator, ByteString], memory: int = None
-    ) -> None:
-        """Will initialize FTDC object and validate the provided input."""
-        if (
-            not isinstance(ftdc_data, Iterator)
-            and not isinstance(ftdc_data, bytes)
-            and not isinstance(ftdc_data, AsyncIterator)
-        ):
-            raise ValueError(
-                "Invalid FTDC data. The FTDC data should either be an"
-                " Iterator/AsyncIterator or ByteString"
-            )
-        self.raw = ftdc_data
-        self.memory = memory
+    # The metrics chunk, which is *not* bson, first
+    # contains a bson document which begins the sample.
+    # We retrieve just that doc. The rest cannot be parsed as bson.
+    # TODO: Ordered dict?
+    ref_doc = bson.decode_file_iter(bson_data).__next__()
+    metrics = _get_metrics_from_ref_doc(ref_doc)
 
-    async def __aiter__(self) -> AsyncGenerator[Dict[Tuple, int], None]:
-        """Will aysnc iterate on the byte string and returing a generator with decoded data."""
-        if not isinstance(self.raw, AsyncIterator):
-            raise ValueError(
-                "Async interation not supported. FTDC data is not of type AsyncIterator"
-            )
-        try:
-            chunk = await self.raw.__anext__()
-        except StopAsyncIteration:
-            return
-        next_chunk = self.raw.__anext__()
-        chunks_created, chunks_awaited = 2, 1
-        gen = self.getftdc(chunk)
-        more = True
-        for x, doc_len, buf in gen:
-            if x is not None:
-                yield x
-            while self.memory is None:
-                if doc_len is not None:
-                    self.memory = 5 * doc_len
-                else:
-                    x, doc_len, buf = gen.send(await next_chunk)
-                    chunks_awaited += 1
-                    next_chunk = self.raw.__anext__()
-                    chunks_created += 1
-                    if x is not None:
-                        yield x
-            if doc_len is not None and self.memory < doc_len:
-                raise Exception(
-                    f"doc_len {doc_len} too large to fit into memory {self.memory}"
-                )
-            if more and (doc_len is None or buf < self.memory):
-                try:
-                    x, doc_len, buf = gen.send(await next_chunk)
-                    chunks_awaited += 1
-                    next_chunk = self.raw.__anext__()
-                    chunks_created += 1
-                    if x is not None:
-                        yield x
-                    while buf < self.memory:
-                        x, doc_len, buf = gen.send(await next_chunk)
-                        if x is not None:
-                            yield x
-                        chunks_awaited += 1
-                        next_chunk = self.raw.__anext__()
-                        chunks_created += 1
-                except StopAsyncIteration:
-                    more = False
-                except Exception:
-                    raise
+    # The next four bytes tell us how many metrics (fields) to expect per event in this chunk.
+    # If that doesn't match the reference doc, error out.
+    metric_count = int.from_bytes(bson_data.read(4), byteorder="little", signed=False)
+    if metric_count != len(metrics):
+        raise ValueError("Metrics mismatch, file likely corrupted."
+                            f" Expected {metric_count} metrics but found {len(ref_doc)}")
 
-    def __iter__(self) -> Generator[Dict[Tuple, int], None, None]:
-        """Will iterate on the byte string and returing a generator with decoded data."""
-        if isinstance(self.raw, bytes):
-            gen = self.getftdc(self.raw)
-            for x, _doc_len, _buf in gen:
-                if x is not None:
-                    yield x
-        elif isinstance(self.raw, Iterator):
-            try:
-                chunk = next(self.raw)
-            except StopIteration:
-                return
-            gen = self.getftdc(chunk)
-            more = True
-            for x, doc_len, buf in gen:
-                if x is not None:
-                    yield x
-                while self.memory is None:
-                    if doc_len is not None:
-                        self.memory = 5 * doc_len
-                    else:
-                        x, doc_len, buf = gen.send(next(self.raw))
-                        if x is not None:
-                            yield x
-                if doc_len is not None and self.memory < doc_len:
-                    raise Exception(
-                        f"doc_len {doc_len} too large to fit into memory {self.memory}"
-                    )
-                if more and (doc_len is None or buf < self.memory):
-                    try:
-                        x, doc_len, buf = gen.send(next(self.raw))
-                        while buf < self.memory:
-                            if x is not None:
-                                yield x
-                            x, doc_len, buf = gen.send(next(self.raw))
-                        if x is not None:
-                            yield x
-                    except StopIteration:
-                        more = False
-                    except Exception:
-                        raise
+    # The next four bytes tell how many delta-encoded events to expect for each series
+    # in this chunk.
+    delta_count = int.from_bytes(bson_data.read(4), byteorder="little", signed=False)
 
-    def getftdc(
-        self, chunk: Union[Chunk, bytes]
-    ) -> Generator[
-        Tuple[Union[Dict[Tuple, int], None], Union[None, int], int], Chunk, None
-    ]:
-        """
-        Get ftdc data from a chunk.
-
-        :param chunk: The chunk to decode.
-        """
-        frame = bytearray()
-        frame.extend(chunk)
-        while True:
-            chunk_doc, frame = self._read_bson_doc(frame)
-            if b"type" in chunk_doc and chunk_doc[b"type"] == 1:
-                for x in self._decode_chunk(chunk_doc):
-                    chunk = yield x, chunk_doc.bson_len, len(frame)
-                    if chunk is not None:
-                        frame.extend(chunk)
-            elif "bson_len" in chunk_doc:
-                chunk = yield None, chunk_doc.bson_len, len(frame)
-                if chunk is not None:
-                    frame.extend(chunk)
-                else:
-                    break
+    # The rest of the chunk is a bunch of varint-encoded deltas for each metric in order.
+    zero_count = 0
+    for metric in metrics:
+        previous_val = metric.reference_val
+        for _ in range(delta_count):
+            if zero_count != 0:
+                delta = 0
+                zero_count -= 1
             else:
-                chunk = yield None, None, len(frame)
-                if chunk is not None:
-                    frame.extend(chunk)
-                else:
-                    break
+                delta = _decode_varint(bson_data)
+                if delta == 0:
+                    zero_count = _decode_varint(bson_data)
+            cur_val = previous_val + metric.delta_constructor(delta)
+            metric.values.append(cur_val)
+            previous_val = cur_val
 
-    def _read_bson_doc(
-        self, frame: bytearray, ftdc: bool = False
-    ) -> Tuple[Chunk, bytearray]:
-        if len(frame) < 4:
-            return Chunk(), frame
-        doc_len = _int32.unpack_from(frame, 0)[0]
-        doc = Chunk()
-        doc.bson_len = doc_len
-        while len(frame) < doc_len:
-            return doc, frame
-        next_frame = frame[doc_len:]
-        frame = frame[4:doc_len]
-        while len(frame) > 0:
-            bson_type = frame[0]
+    if bson_data.read():
+        raise ValueError("Metrics chunk contains extra deltas. Expected only {delta_count}")
 
-            frame = frame[1:]
-            name_end = frame.find(b"\0", 0)
-            n = frame[0:name_end]
-            frame = frame[name_end + 1 :]
-            if bson_type == 0:  # eoo
-                return doc, next_frame
-            elif bson_type == 1:  # _double
-                v = _double.unpack_from(frame, 0)[0]
-                if ftdc:
-                    v = int(v)
-                frame = frame[8:]
-            elif bson_type == 2:  # string
-                p = _uint32.unpack_from(frame, 0)[0]
-                frame = frame[4:]
-                v = frame[: p - 1] if not ftdc else None
-                frame = frame[p:]
-            elif bson_type == 3:  # subdoc
-                v, _ = self._read_bson_doc(frame, ftdc=ftdc)
-                frame = frame[v.bson_len :]
-            elif bson_type == 4:  # array
-                v, _ = self._read_bson_doc(frame, ftdc=ftdc)
-                frame = frame[v.bson_len :]
+    yield ref_doc
 
-                if not ftdc:
-                    v = list(v.values())  # return as array
-            elif bson_type == 8:  # bool
-                v = frame[0]
-                frame = frame[1:]
-            elif bson_type == 5:  # bindata
-                p = _uint32.unpack_from(frame, 0)[0]
-                frame = frame[5:]
-                v = frame[:p] if not ftdc else None
-                frame = frame[p:]
-            elif bson_type == 7:  # objectid
-                v = None  # xxx always ignore for now
-                frame = frame[12:]
-            elif bson_type == 9:  # datetime
-                v = _uint64.unpack_from(frame, 0)[0]
-                v = int(v) if ftdc else v / 1000.0
-                frame = frame[8:]
-            elif bson_type == 16:  # _int32
-                v = _int32.unpack_from(frame, 0)[0]
-                if ftdc:
-                    v = int(v)
-                frame = frame[4:]
-            elif bson_type == 17:  # timestamp
-                v = Chunk()
-                v[b"t"] = int(_uint32.unpack_from(frame, 0)[0])  # seconds
-                v[b"i"] = int(_uint32.unpack_from(frame, 4)[0])  # increment
-                frame = frame[8:]
-            elif bson_type == 18:  # _int64
-                v = int(_int64.unpack_from(frame, 0)[0])
-                frame = frame[8:]
-            elif bson_type == 0xFF or bson_type == 0x7F:  # minkey, maxkey
-                v = None  # xxx always ignore for now
-                p = 0
-            else:
-                err_msg = "unknown type %d(%x) at %d(%x)"
-                raise Exception(err_msg % (bson_type, bson_type))
-            if v is not None:
-                doc[bytes(n)] = v
-        # assert not "eoo not found"  # should have seen an eoo and returned
-        return doc, next_frame  # mypy cannot resolve this as unreachable
+    for i in range(delta_count):
+        doc = {}
+        for metric in metrics:
+            cur = doc
+            for j in range(len(metric.key_path)-1):
+                key = metric.key_path[j]
+                if key not in cur:
+                    cur[key] = {}
+                cur = cur[key]
+            cur[metric.key_path[-1]] = metric.values[i]
+        yield doc
 
-    def _decode_chunk(self, chunk_doc: Chunk) -> Generator[dict, None, None]:
-        # our result is a map from metric keys to list of values for each metric key
-        # a metric key is a path through the sample document represented as a tuple
-        metrics = Chunk()
 
-        # decompress chunk data field
-        data = chunk_doc[b"data"]
-        metrics.chunk_len = len(data)
-        data = data[4:]  # skip uncompressed length, we don't need it
-        data = zlib.decompress(data)
-        # read reference doc from chunk data, ignoring non-metric fields
-        ref_doc, _ = self._read_bson_doc(bytearray(data), ftdc=True)
+def _decode_varint(data_stream):
+    """
+    Magically decode a varint.
 
-        metrics_init = {}
+    More notes about varints: https://developers.google.com/protocol-buffers/docs/encoding
+    """
+    res = 0
+    shift = 0
+    while True:
+        b = int.from_bytes(data_stream.read(1), byteorder="little")
+        res |= (b & 0x7F) << shift
+        if not (b & 0x80):
+            if res > 0x7FFFFFFFFFFFFFFF:  # negative 64-bit value
+                res = int(res - 0x10000000000000000)
+            return res
+        shift += 7
 
-        # traverse the reference document and extract map from metrics keys to values
-        def extract_keys(doc: Chunk, n: Tuple = ()) -> None:
-            for k, v in doc.items():
-                nn = n + (k,)
-                if type(v) == Chunk:
-                    extract_keys(v, nn)
-                else:
-                    metrics_init[nn] = v
 
-        extract_keys(ref_doc)
-        if len(metrics_init.keys()) == 0:
-            return
+def _get_metrics_from_ref_doc(ref_doc):
+    metrics = []
+    key_path = []
+    for key_path, value in _get_paths_and_vals(ref_doc):
+        bson_translate, delta_constructor = _get_constructors_for_val(value)
+        metrics.append(MetricChunk(key_path=key_path, values=[],
+                              reference_val=bson_translate(value),
+                              delta_constructor=delta_constructor))
+    return metrics
+
+def _get_paths_and_vals(d, key_path=[]):
+    output = []
+    for key, value in d.items():
+        key_path.append(key)
+        if isinstance(value, MutableMapping):
+            output += _get_paths_and_vals(value, key_path)
         else:
-            # get nmetrics, ndeltas
-            nmetrics = _uint32.unpack_from(data, ref_doc.bson_len)[0]
-            ndeltas = _uint32.unpack_from(data, ref_doc.bson_len + 4)[0]
-            nsamples = ndeltas + 1
-            at = ref_doc.bson_len + 8
-            if nmetrics != len(metrics_init):
-                raise RuntimeError(
-                    "Bad chunk found, but application never supported it"
-                )
-            metrics.nsamples = nsamples
+            output.append((tuple(key_path), value))
+        key_path.pop()
+    return output
 
-            # unpacks ftdc packed ints
-            def unpack(data: ByteString, at: int) -> Tuple[int, int]:
-                res = 0
-                shift = 0
-                while True:
-                    b = data[at]
-                    res |= (b & 0x7F) << shift
-                    at += 1
-                    if not (b & 0x80):
-                        if res > 0x7FFFFFFFFFFFFFFF:  # negative 64-bit value
-                            res = int(res - 0x10000000000000000)
-                        return res, at
-                    shift += 7
 
-            # unpack, run-length, delta, transpose the metrics
-            nzeroes = 0
-            ftdc_record: Dict[int, Dict] = {}
-            yield metrics_init
-            for k, v in metrics_init.items():
-                value = v
-                for i in range(ndeltas):
-                    if nzeroes:
-                        delta = 0
-                        nzeroes -= 1
-                    else:
-                        delta, at = unpack(data, at)
-                        if delta == 0:
-                            nzeroes, at = unpack(data, at)
-                    value += delta
-                    if i + 1 not in ftdc_record:
-                        ftdc_record[i + 1] = {}
-                    ftdc_record[i + 1][k] = value
+def _get_constructors_for_val(val):
+    """
+    Return two functions:
 
-            for x in ftdc_record.values():
-                yield x
+    One for converting the initial bson value to a pythonic value.
+
+    Another for converting integer delta values to that type's delta.
+    """
+
+    def identity(x):
+        return x
+
+    def int_to_timedelta(x):
+        return datetime.timedelta(milliseconds=x)
+
+    # TODO: There are more possible bson, and therefore likely possible FTDC types.
+    # These are just the ones that Genny & Poplar metrics outputs use. We will want
+    # to consider expanding this in the future.
+    if isinstance(val, datetime.datetime):
+        return identity, int_to_timedelta
+    elif isinstance(val, bson.int64.Int64):
+        return int, identity
+    elif isinstance(val, bool):
+        return identity, bool
+    else:
+        raise ValueError(f"Unknown type found in FTDC chunk reference doc: {type(val)}")
+    
+
+def _get_metrics(d, parent_keys):
+    items = []
+    for key, value in to_flatten.items():
+        new_key = f"{parent_key}{sep}{key}" if parent_key else key
+        if isinstance(value, MutableMapping):
+            items.extend(_flatten_dict(value, new_key, sep=sep).items())
+        else:
+            items.append((new_key, value))
+    return dict(items)
+
+
+
+# Note for somewhere: BSON only has millisecond-granularity for timestamps. So even though
+# genny reports TS using milliseconds, that seems to be lost when dumped to disk.
