3bd53571ed70532dcfda3301a7ac3e6023f2321b
Raiden Worley <carl.worley@mongodb.com>
2022-10-03 13:58:08 -0400

    better testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,23 +7,23 @@ import pytest
 def test_data() -> List[Dict[str, object]]:
     return [
         {
-            "ftdc_file": "./tests/test_bson_files/test_data_ftdc_file_empty_doc",
+            "ftdc_file": "./tests/test_files/ftdc/test_data_ftdc_file_empty_doc",
+            "json_file": "./tests/test_files/json/test_data_ftdc_file_empty_doc",
             "doc_count": 0,
         },
         {
-            "ftdc_file": "./tests/test_bson_files/test_data_ftdc_file_0_doc",
+            "ftdc_file": "./tests/test_files/ftdc/test_data_ftdc_file_0_doc",
+            "json_file": "./tests/test_files/json/test_data_ftdc_file_0_doc",
             "doc_count": 1,
         },
         {
-            "ftdc_file": "./tests/test_bson_files/test_data_ftdc_file_1_doc",
+            "ftdc_file": "./tests/test_files/ftdc/test_data_ftdc_file_1_doc",
+            "json_file": "./tests/test_files/json/test_data_ftdc_file_1_doc",
             "doc_count": 1,
         },
         {
-            "ftdc_file": "./tests/test_bson_files/test_data_ftdc_file_101_doc",
+            "ftdc_file": "./tests/test_files/ftdc/test_data_ftdc_file_101_doc",
+            "json_file": "./tests/test_files/json/test_data_ftdc_file_101_doc",
             "doc_count": 101,
-        },
-        {
-            "ftdc_file": "./tests/test_bson_files/test_data_ftdc_file_1102029_doc",
-            "doc_count": 1102029,
         },
     ]
--- tests/test_ftdc_decode.py
+++ tests/test_ftdc_decode.py
@@ -1,3 +1,6 @@
+import json
+
+from datetime import timezone
 from typing import Dict, List
 
 from src.ftdc_tools.decode import decode_file_iter
@@ -7,39 +10,26 @@ from src.ftdc_tools.decode import decode_file_iter
 # from aiofile import async_open
 
 
-def validate_ftdc_record(record) -> None:
-    """Do a very basic validation of a FTDC record."""
-    # required_keys = [
-    #    ("ts",),
-    #    ("id",),
-    #    ("counters", "n"),
-    #    ("counters", "ops"),
-    #    ("counters", "size"),
-    #    ("counters", "errors"),
-    #    ("timers", "dur"),
-    #    ("timers", "total"),
-    #    ("gauges", "state"),
-    #    ("gauges", "workers"),
-    #    ("gauges", "failed"),
-    # ]
-    # assert len(record) == len(required_keys)
-    # for key in required_keys:
-    #    assert key in record.keys()
-    #    assert type(record[key]) == int
-    top_level_keys = ["ts", "id", "counters", "timers", "gauges"]
-    for index, key in enumerate(record.keys()):
-        assert key == top_level_keys[index]
-
-
 class TestFTDCDecode:
     def test_ftdc_rollup_file_iter(self, test_data: List[Dict]) -> None:
         for test in test_data:
-            with open(test["ftdc_file"], "rb") as f:
-                record_count = 0
-                for ftdc_row in decode_file_iter(f):
-                    validate_ftdc_record(ftdc_row)
-                    record_count += 1
-            assert record_count == test["doc_count"]
+            with open(test["ftdc_file"], "rb") as f, open(test["json_file"], "r") as j:
+                index = 0
+                print("json file: ", test["json_file"])
+                for ftdc_doc in decode_file_iter(f):
+
+                    json_doc = json.loads(j.readline())
+                    # This converts nested OrderedDicts to dicts.
+                    # We can't just load the json file as an OrderedDict because apparently
+                    # curator exports fields in a different order than pymongo. (I'd defer to pymongo.)
+                    print(ftdc_doc["ts"].timestamp())
+                    ftdc_doc["ts"] = int(
+                        ftdc_doc["ts"].replace(tzinfo=timezone.utc).timestamp() * 1000
+                    )  # - 18000000  # Microsecond conversion
+                    ftdc_doc = json.loads(json.dumps(ftdc_doc))
+                    assert ftdc_doc == json_doc
+                    index += 1
+                assert not j.read(1)
 
     # def test_ftdc_rollup_streaming(self, test_data: List[Dict]) -> None:
     #    for test in test_data:
