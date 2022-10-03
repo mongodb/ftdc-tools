0ed8f5b577268ff3ecb728fa444c52c538805b36
Raiden Worley <carl.worley@mongodb.com>
2022-10-03 15:46:33 -0400

    test get metrics

--- tests/test_ftdc_decode.py
+++ tests/test_ftdc_decode.py
@@ -3,7 +3,14 @@ import json
 from datetime import timezone
 from typing import Any, Dict, List
 
-from src.ftdc_tools.decode import decode_file_iter, decode_iter
+import bson
+
+from src.ftdc_tools.decode import (
+    MetricChunk,
+    _get_metrics_from_ref_doc,
+    decode_file_iter,
+    decode_iter,
+)
 
 
 def test_ftdc_decode_file_iter(test_data: List[Dict]) -> None:
@@ -30,6 +37,45 @@ def test_ftdc_decode_iter(test_data: List[Dict]) -> None:
                 assert ftdc_doc == json_doc
                 index += 1
             assert not j.read(1)  # Assert no more json.
+
+
+def test_get_metrics_from_ref_doc() -> None:
+    ref_doc = {
+        "metricA": bson.int64.Int64(5),
+        "metricB": bson.int64.Int64(6),
+        "nestedMetric": {
+            "metricC": bson.int64.Int64(7),
+            "subnestedMetric": {"metricD": True},
+        },
+    }
+
+    expected = [
+        MetricChunk(key_path=("metricA",), values=[5], delta_constructor=lambda x: x),
+        MetricChunk(key_path=("metricB",), values=[6], delta_constructor=lambda x: x),
+        MetricChunk(
+            key_path=("nestedMetric", "metricC"),
+            values=[7],
+            delta_constructor=lambda x: x,
+        ),
+        MetricChunk(
+            key_path=("nestedMetric", "subnestedMetric", "metricD"),
+            values=[True],
+            delta_constructor=lambda x: x,
+        ),
+    ]
+    actual = _get_metrics_from_ref_doc(ref_doc)
+    # Can't put the identity nested function in the function under test into the expected list.
+    assert len(expected) == len(actual)
+    for i, expected_chunk in enumerate(expected):
+        assert expected_chunk.key_path == actual[i].key_path
+        assert expected_chunk.values == actual[i].values
+        assert expected_chunk.delta_constructor(
+            756
+        ) == expected_chunk.delta_constructor(756)
+
+
+def test_get_metrics_from_empty_ref_doc() -> None:
+    assert _get_metrics_from_ref_doc({}) == []
 
 
 def ftdc_to_dict(ftdc_doc) -> Dict[str, Any]:
